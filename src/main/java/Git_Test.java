ba2bd8e89d851b2f2c4ffa3a452d9f4b10487b65
lidianyangha <94355833+lidianyangha@users.noreply.github.com>
2022-05-28 14:01:38 +0800

    commit one

---
+++ src/main/java/Git_Test.java
@@ -0,0 +1,13 @@
+/**
+ * @Author LiDianyang
+ * @Date 2022/5/28 13:49
+ * @PackageName:PACKAGE_NAME
+ * @ClassName: Git_Test
+ * @Description: TODO
+ * @Version 1.0
+ */
+public class Git_Test {
+    public static void main(String[] args){
+        System.out.println("Test_Git");
+    }
+}
